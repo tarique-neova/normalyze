b974aa0fe005d774bd641a46415ad6a5480db634
tarique-neova <tarique_salat@neovasolutions.in>
2024-06-10 18:23:33 +0530

    updated code

--- normalyze-automation/compliance-automation/src/main/compliance/generator/cloud/azure/terraform/data.tf
+++ normalyze-automation/compliance-automation/src/main/compliance/generator/cloud/azure/terraform/data.tf
@@ -1,6 +1,3 @@
-# data "azurerm_client_config" "current" {
-# }
-
 data "external" "current_ip" {
   program = ["bash", "${path.module}/scripts/get_ip.sh"]
 }
@@ -8,29 +5,3 @@ data "external" "current_ip" {
 data "external" "random_value" {
   program = ["bash", "scripts/generate_random_value.sh"]
 }
-
-# # Data retrieval from Key Vault
-# data "azurerm_key_vault" "kv" {
-#   name                = "tsalat"
-#   resource_group_name = "tsalat-test"
-# }
-
-# data "azurerm_key_vault_secret" "subscription_id" {
-#   name         = "subscription-id"
-#   key_vault_id = data.azurerm_key_vault.kv.id
-# }
-
-# data "azurerm_key_vault_secret" "client_id" {
-#   name         = "client-id"
-#   key_vault_id = data.azurerm_key_vault.kv.id
-# }
-
-# data "azurerm_key_vault_secret" "client_secret" {
-#   name         = "client-secret"
-#   key_vault_id = data.azurerm_key_vault.kv.id
-# }
-
-# data "azurerm_key_vault_secret" "tenant_id" {
-#   name         = "tenant-id"
-#   key_vault_id = data.azurerm_key_vault.kv.id
-# }
--- normalyze-automation/compliance-automation/src/main/compliance/generator/cloud/azure/terraform/locals.tf
+++
@@ -1,7 +0,0 @@
-//List of all locals to be used for Azure resource creation
-#
-# locals {
-#   current_timestamp      = var.current_timestamp_for_debugging == "" ? time_static.current_time.rfc3339 : var.current_timestamp_for_debugging
-#   timestamp_sanitized    = replace(local.current_timestamp, "/[-| |T|Z|:]/", "")
-#   customized_name        = "${var.name_prefix}-${local.timestamp_sanitized}"
-# }
